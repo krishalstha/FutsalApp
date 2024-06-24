0d5267267ac9065cf8f228015966a8f8bfad9ae5
krishalstha <krishalstha989@gmail.com>
2024-06-24 18:53:30 +0545

    hello

---
+++ lib/main.dart
@@ -0,0 +1,181 @@
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'regmain.dart';
+import 'forget.dart';
+import 'home.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyDqs1Vz5eOUliJQPd7knH1Lwevay3eZ_R0",
+      appId: "1:194085522422:android:ce4cc804165c836d9c15d7",
+      messagingSenderId: "194085522422",
+      projectId: "first-login-45e68",
+    ),
+  )
+      : await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Futsal App',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginScreen(),
+        '/registration': (context) => const RegistrationScreen(),
+        '/forget': (context) => const ForgetScreen(),
+        '/home': (context) => const HomeScreen(), // Add route for HomeScreen
+      },
+    );
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[600],
+        title: const Text(
+          'FutsalApp',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.grey[400],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.teal,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'Enter email',
+                      prefixIcon: Icon(Icons.email),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (String value) {},
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please enter email' : null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Enter Password',
+                      prefixIcon: Icon(Icons.password),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (String value) {},
+                    validator: (value) {
+                      return value!.isEmpty ? 'Please enter password' : null;
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, '/forget');
+                    },
+                    child: Text(
+                      'Forget Password?',
+                      style: TextStyle(
+                        fontSize: 17,
+                        color: Colors.red[900],
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      MaterialButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'LogIn',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        color: Colors.green,
+                        minWidth: 100,
+                        height: 40,
+                      ),
+                      const SizedBox(width: 20),
+                      MaterialButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/registration');
+                        },
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        color: Colors.green,
+                        minWidth: 100,
+                        height: 40,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/home');
+                    },
+                    child: const Text(
+                      'GTHomePage',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    color: Colors.green,
+                    minWidth: 100,
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
